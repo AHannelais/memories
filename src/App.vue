239fc1e813caf754f048cf3f6aa9b1eb8f88d82b
AHANNELAIS <antoine.hannelais@gmail.com>
2022-07-31 19:51:58 +0200

    🎉 init vue

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+const count = 0;
+</script>
+
+<template>
+  <div>App</div>
+  <div>{{ count }}</div>
+</template>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [vue()],
   server: { port: 3000 },
+  resolve: {
+    alias: {
+      "@": `${resolve(__dirname, "src")}/`,
+    },
+  },
 });
